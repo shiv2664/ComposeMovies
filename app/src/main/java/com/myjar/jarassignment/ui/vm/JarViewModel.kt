aa38de66d4ce6e624655285ed9b2d8832b4007e6
shiv2664 <shivamsehrawat38@gmail.com>
2025-07-06 22:01:29 +0530

    Initial Commit

---
+++ app/src/main/java/com/myjar/jarassignment/ui/vm/JarViewModel.kt
@@ -0,0 +1,29 @@
+package com.myjar.jarassignment.ui.vm
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.myjar.jarassignment.createRetrofit
+import com.myjar.jarassignment.data.model.ComputerItem
+import com.myjar.jarassignment.data.repository.JarRepository
+import com.myjar.jarassignment.data.repository.JarRepositoryImpl
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class JarViewModel : ViewModel() {
+
+    private val _listStringData = MutableStateFlow<List<ComputerItem>>(emptyList())
+    val listStringData: StateFlow<List<ComputerItem>>
+        get() = _listStringData
+
+    private val repository: JarRepository = JarRepositoryImpl(createRetrofit())
+
+    fun fetchData() {
+        viewModelScope.launch {
+            repository.fetchResults().collect { result ->
+                    _listStringData.value = result
+                }
+        }
+    }
+}
